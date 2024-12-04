4359957745f5e2a1b1466ec9dd0483d7238bc625
Ana Cvasniuc <annycvasniuc@yahoo.com>
2024-12-04 10:27:53 +0100

    update pods

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
 
 final formatter = DateFormat.yMd();
@@ -23,13 +26,49 @@ class Expense {
   }) : id = uuid
             .v4(); // quando questa classe viene inizializzata, al uuid li assegniamo un valore univoco creato sopra
 
-  final String id;
-  final String title;
-  final double amount;
-  final DateTime date;
-  final Category category; // leisure
+  String id;
+  String title;
+  double amount;
+  DateTime date;
+  Category category; // leisure
 
   String get formatteDate {
     return formatter.format(date);
+  }
+
+  Expense.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        title = json['title'],
+        amount = json['amount'],
+        date = json['date'],
+        category = json['category'];
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'title': title,
+        'amount': amount,
+        'date': date,
+        'category': category,
+      };
+
+  // ···
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    //("directory.path -- " + directory.path);
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/expenses.txt');
+  }
+
+// write data to the file
+  Future<File> writeCounter(int expenses) async {
+    final file = await _localFile;
+
+    // Write the file
+    return file.writeAsString('$expenses');
   }
 }
---
+++ lib/widgets/file_manager.dart
@@ -0,0 +1,9 @@
+/*class FileManager {
+  static FileManager _instance;
+
+  FileManager.internal() {
+    _instance = this;
+  }
+
+  factory FileManager() => _instance ?? FileManager._internal();
+}*/
